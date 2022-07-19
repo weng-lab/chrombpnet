838d2dd038685e532e68e37276417c0e96134438
soumyakundu <soumya.kundu@stanford.edu>
2022-07-18 22:43:26 -0700

    Updated splits script

--- chrombpnet/helpers/make_chr_splits/splits.py
+++ chrombpnet/helpers/make_chr_splits/splits.py
@@ -1,43 +1,43 @@
 import json
 import argparse
 import os
+import random
+from chrombpnet.training.utils import data_utils 
 
 def main(): 
     parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--genome", type=str, required=True, help="Genome fasta")
+    parser.add_argument("-b", "--bigwig", type=str, required=True, help="Bigwig of tn5 insertions")
+    parser.add_argument("-p", "--peaks", type=str, required=True, help="Peaks")
+    parser.add_argument("-n", "--nonpeaks", type=str, required=True, help="Non-Peaks")
+    parser.add_argument("-il", "--inputlen", type=int, default=2114, help="Sequence input length")
+    parser.add_argument("-ol", "--outputlen", type=int, default=1000, help="Prediction output length")
+    parser.add_argument("-j", "--max_jitter", type=int, default=0, help="Maximum jitter applied on either side of region")
     parser.add_argument("-o", "--output_dir", type=str, required=True, help="Path to store the fold information")
     args = parser.parse_args()
 
-    # mention what the outputformat should look like for further editing
+    peak_regions = pd.read_csv(args.peaks, header=None, sep='\t')
+    nonpeak_regions = pd.read_csv(args.nonpeaks, header=None, sep='\t')
 
-    chroms = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8', 'chr9','chr10', 'chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY']
-    splits=dict()
+    peak_seqs, peak_cts, peak_coords, nonpeak_seqs, nonpeak_cts, nonpeak_coords, = data_utils.load_data(peak_regions, nonpeak_regions,
+                                                                                                        args.genome, args.bigwig,
+                                                                                                        args.inputlen, args.outputlen,
+                                                                                                        args.max_jitter)
 
-    splits[0]={'test':['chr1', 'chr3', 'chr6'],
-               'valid':['chr8', 'chr20']}
-    splits[0]['train'] = [chrom for chrom in chroms if chrom not in splits[0]['test']+splits[0]['valid']]
-    json.dump(splits[0], open(os.path.join(args.output_dir, "fold_0.json"),"w"), indent=4)
+    peak_df = pd.DataFrame({'coords': peak_coords, 'cts': peak_cts, 'fold0': [], 'fold1': [], 'fold2': [], 'fold3': [], 'fold4': []})
+    peak_df.sort_values(by='cts', inplace=True)
 
-    splits[1]={'test':['chr2', 'chr8', 'chr9', 'chr16'],
-               'valid':[ 'chr12', 'chr17']}
-    splits[1]['train'] = [chrom for chrom in chroms if chrom not in splits[1]['test']+splits[1]['valid']]
-    json.dump(splits[1], open(os.path.join(args.output_dir, "fold_1.json"),"w"), indent=4)
+    for index,row in peak_df.iterrows():
+        test_or_valid = random.choice(['valid', 'test'])
+        test_or_valid_fold = random.choice(range(5))
+        for fold in range(5):
+            if fold != test_or_valid_fold:
+                peak_df['fold' + str(fold)].append('train')
+            else:
+                peak_df['fold' + str(fold)].append(test_or_valid)
 
-    splits[2]={'test':['chr4','chr11', 'chr12', 'chr15', 'chrY'],
-               'valid':['chr22','chr7']}
-    splits[2]['train'] = [chrom for chrom in chroms if chrom not in splits[2]['test']+splits[2]['valid']]
-    json.dump(splits[2], open(os.path.join(args.output_dir, "fold_2.json"),"w"), indent=4)
-
-
-    splits[3]={'test':['chr5','chr10','chr14', 'chr18', 'chr20', 'chr22'],
-               'valid':['chr6', 'chr21']}
-    splits[3]['train'] = [chrom for chrom in chroms if chrom not in splits[3]['test']+splits[3]['valid']]
-    json.dump(splits[3], open(os.path.join(args.output_dir, "fold_3.json"),"w"), indent=4)
-
-
-    splits[4]={'test':['chr7','chr13','chr17', 'chr19', 'chr21', 'chrX'],
-               'valid':['chr10','chr18']}
-    splits[4]['train'] = [chrom for chrom in chroms if chrom not in splits[4]['test']+splits[4]['valid']]
-    json.dump(splits[4], open(os.path.join(args.output_dir, "fold_4.json"),"w"), indent=4)
+    
+    peak_df.to_csv(args.outdir + '/splits.tsv', sep='\t')
 
 if __name__=="__main__":
     main()
